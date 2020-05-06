5945866e4781bd11b4e8c10a091ba81823048e71
Morgan <morgan.batterham@gmail.com>
2020-05-06 15:47:16 +1000

    initialised database

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :weathers
+  get "/", to: "weather#home"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
